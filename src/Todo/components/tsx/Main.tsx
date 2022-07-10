239010abb1d9c93266cbfa875b12861177f3cd69
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-07-10 16:57:26 +0500

    commit_20

--- src/Todo/components/tsx/Main.tsx
+++ src/Todo/components/tsx/Main.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useMemo } from "react";
 import { IChildrens, IMain } from "./interfaces";
 import styles from "../css/todo.module.css"
 import { useTodoList } from "./hooks/useTodoList";
@@ -27,13 +27,14 @@ export const Main: FC<IMain> = React.memo(({ }) => {
 	const { selectRedactItemFunc, redactItemChange, redactItem, itemCategoryRedact, redactItemFunc, redactTextChange, cleanRedactItems } = useRedactItem({ todoList, setTodoList })
 	const { inputSearchChange, inputSearchValue, filterCategoryTodo, selectChange, selectedOption } = useInputSearch({ todoList, setTodoList })
 	const childrensArray = childrensArrayFunc({ itemCategoryRedact, selectChange, selectedOption, inputSearchValue, inputSearchChange, redactTextChange, redactItemFunc, cleanRedactItems, redactItem, redactItemChange, inputTextValue, inputCategoryChange, category, addCategories, inputTitleValue, inputTitleChange, addItem, selectedCategory, categories, itemCategory, inputTextChange })
+	const fields = useMemo(() => childrensArray.map((x, idx) => <Fields key={idx} childrens={x} />), [selectedOption, inputSearchValue, redactItem, inputTextValue, category, inputTitleValue, selectedCategory, categories])
 	return (
 		//provider
 		<div className={[styles.main, "p-5 "].join(" ")}>
 			<div className={styles.container} >
 				<div>Info...</div>
 				{/*  one component */}
-				{childrensArray.map((x, idx) => <Fields key={idx} childrens={x} />)}
+				{fields}
 				{/* one component */}
 				<ContOutPutList todoLength={todoList.length} searchFilterTodo={filterCategoryTodo} removeItem={removeItem} redactItem={selectRedactItemFunc} confirmation={confirmation} onClick={removeItemConfirmation} />
 			</div>
