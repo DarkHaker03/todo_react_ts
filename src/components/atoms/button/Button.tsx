635fceda96cdca2e608469e25c4d79c244d6b58c
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-07-12 21:03:40 +0500

    add!: added eslint config

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+
 import { IButton } from "../../interfaces"
 import styles from "./button.module.css"
 export const Button: FC<IButton> = React.memo(({ onClick, children }) => {
