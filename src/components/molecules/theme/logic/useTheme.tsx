9cec66317c0cc1cc1ae09a1e33186addba3424ce
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-07-17 14:13:10 +0500

    fix: taken out logic of themeChange from useTheme in individual hook useThemeChange

--- src/components/molecules/theme/logic/useTheme.tsx
+++ src/components/molecules/theme/logic/useTheme.tsx
@@ -3,6 +3,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import CSS from 'csstype';
 
 import { useThemeLocalStorage } from './useThemeLocalStorage';
+import { useThemeChange } from './useThemeChange';
 
 export const useTheme = () => {
 	const [theme, setTheme] = useState<CSS.Properties>({});
@@ -17,64 +18,6 @@ export const useTheme = () => {
 		}
 		setWhatChange(value);
 	}, [whatChange]);
-	const themeChange = useCallback((event: React.MouseEvent<HTMLDivElement>) => {
-		const value = event.currentTarget.style;
-		if (whatChange === 'text') {
-			setTheme({ ...theme, color: value.backgroundColor });
-		} else if (whatChange === 'backGround') {
-			setTheme({ ...theme, backgroundColor: value.backgroundColor });
-		} else if (whatChange === 'items') {
-			setItemsStyle({
-				...itemsStyle,
-				color: `
-			select{
-				background-color: ${value.backgroundColor} !important;
-				
-			}
-			.m_select{
-				background-color: ${value.backgroundColor};
-				
-			}
-			.select__item{
-				background-color: ${value.backgroundColor};
-				
-			}
-			button{
-				background-color:  ${value.backgroundColor}!important;
-			}
-			input{
-				background-color:  ${value.backgroundColor}!important;
-			}
-			textArea{
-				background-color:  ${value.backgroundColor}!important;
-			}
-			`});
-		} else if (whatChange === 'textInItems') {
-			setItemsStyle({
-				...itemsStyle,
-				background: `
-			select{
-				color: ${value.backgroundColor} !important;
-				
-			}
-			.m_select{
-				color: ${value.backgroundColor} ;
-				
-			}
-			.select__item{
-				color: ${value.backgroundColor} ;
-			}
-			button{
-				color:  ${value.backgroundColor}!important;
-			}
-			input{
-				color:  ${value.backgroundColor}!important;
-			}
-			textArea{
-				color:  ${value.backgroundColor}!important;
-			}
-			`});
-		}
-	}, [whatChange]);
+	const { themeChange } = useThemeChange({ whatChange, setTheme, setItemsStyle, itemsStyle, theme })
 	return { theme, itemsStyle, themeChange, whatChangeFunc, whatChange };
 };
---
+++ src/components/molecules/theme/logic/useThemeChange.ts
@@ -0,0 +1,82 @@
+import { useCallback } from "react";
+
+import CSS from 'csstype';
+
+interface IUseThemeChange {
+	whatChange: string,
+	setItemsStyle: React.Dispatch<React.SetStateAction<{
+		background: string;
+		color: string;
+	}>>,
+	setTheme: React.Dispatch<React.SetStateAction<CSS.Properties<0 | (string & {}), string & {}>>>,
+	itemsStyle: {
+		background: string;
+		color: string;
+	},
+	theme: CSS.Properties<0 | (string & {}), string & {}>
+}
+
+
+
+export const useThemeChange = ({ whatChange, setTheme, setItemsStyle, itemsStyle, theme }: IUseThemeChange) => {
+	const themeChange = useCallback((event: React.MouseEvent<HTMLDivElement>) => {
+		const value = event.currentTarget.style;
+		if (whatChange === 'text') {
+			setTheme({ ...theme, color: value.backgroundColor });
+		} else if (whatChange === 'backGround') {
+			setTheme({ ...theme, backgroundColor: value.backgroundColor });
+		} else if (whatChange === 'items') {
+			setItemsStyle({
+				...itemsStyle,
+				color: `
+			select{
+				background-color: ${value.backgroundColor} !important;
+				
+			}
+			.m_select{
+				background-color: ${value.backgroundColor};
+				
+			}
+			.select__item{
+				background-color: ${value.backgroundColor};
+				
+			}
+			button{
+				background-color:  ${value.backgroundColor}!important;
+			}
+			input{
+				background-color:  ${value.backgroundColor}!important;
+			}
+			textArea{
+				background-color:  ${value.backgroundColor}!important;
+			}
+			`});
+		} else if (whatChange === 'textInItems') {
+			setItemsStyle({
+				...itemsStyle,
+				background: `
+			select{
+				color: ${value.backgroundColor} !important;
+				
+			}
+			.m_select{
+				color: ${value.backgroundColor} ;
+				
+			}
+			.select__item{
+				color: ${value.backgroundColor} ;
+			}
+			button{
+				color:  ${value.backgroundColor}!important;
+			}
+			input{
+				color:  ${value.backgroundColor}!important;
+			}
+			textArea{
+				color:  ${value.backgroundColor}!important;
+			}
+			`});
+		}
+	}, [whatChange]);
+	return { themeChange }
+}
