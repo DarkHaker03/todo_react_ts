d882752da107f318ed4bc13be7c821c7acc49492
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-07-27 12:14:12 +0500

    delete: deleted comments in theme/logic theme

--- src/components/molecules/theme/logic/theme.ts
+++ src/components/molecules/theme/logic/theme.ts
@@ -1,13 +1,10 @@
 import { createEvent, createStore, sample } from 'effector';
 
 import CSS from 'csstype';
-// import { useCallback } from 'react';
-// import { themeLocalStorageStartFx } from './themeLocalStorage';
 
 export const setTheme = createEvent<CSS.Properties>()
 export const setItemsStyle = createEvent<{ background: string, color: string }>()
 export const setWhatChange = createEvent<string>()
-
 
 export const $theme = createStore<CSS.Properties>({})
 	.on(setTheme, (_, state) => state)
@@ -17,9 +14,6 @@ export const $itemStyle = createStore<{ background: string, color: string }>({ b
 
 export const $whatChange = createStore<string>('')
 	.on(setWhatChange, (_, state) => state)
-
-// themeLocalStorageStartFx()
-
 
 export const whatChangeFunc = (value: string) => {
 	if (value === 'default') {
