aa57052f9d8a75bc62a9e9d6fd10f493c6c97988
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-07-27 21:06:56 +0500

    refactor: refactored logic theme in theme/logic

--- src/components/molecules/theme/logic/theme.ts
+++ src/components/molecules/theme/logic/theme.ts
@@ -6,17 +6,15 @@ export const setTheme = createEvent<CSS.Properties>()
 export const setItemsStyle = createEvent<{ background: string, color: string }>()
 export const setWhatChange = createEvent<string>()
 
-export const $theme = createStore<CSS.Properties>({})
-	.on(setTheme, (_, state) => {
-		return JSON.stringify(_) === JSON.stringify(state) ? _ : state
-	})
+const objectCheck = (_: object, state: object) => {
+	return JSON.stringify(_) !== JSON.stringify(state)
+}
 
-export const $itemStyle = createStore<{ background: string, color: string }>({ background: '', color: '' })
-	.on(setItemsStyle, (_, state) => {
-		return JSON.stringify(_) === JSON.stringify(state) ? _ : state
-	})
+export const $theme = createStore<CSS.Properties>({}, { updateFilter: objectCheck })
+	.on(setTheme, (_, state) => state)
+
+export const $itemStyle = createStore<{ background: string, color: string }>({ background: '', color: '' }, { updateFilter: objectCheck })
+	.on(setItemsStyle, (_, state) => state)
 
 export const $whatChange = createStore<string>('')
-	.on(setWhatChange, (_, state) => {
-		return JSON.stringify(_) === JSON.stringify(state) ? _ : state
-	})
+	.on(setWhatChange, (_, state) => state)
