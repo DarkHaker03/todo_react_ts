ac1274b4d57008be800f81178d13dbf3f9ee5fd0
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-07-29 14:30:07 +0500

    change: changed logic of todoListLocalStorage

--- src/global/store/todoList/todoListLocalStorage.ts
+++ src/global/store/todoList/todoListLocalStorage.ts
@@ -4,14 +4,12 @@ import { IItemTodo } from '../../../components/molecules/itemTodo/ItemTodo';
 
 import { $todoList, setFullTodoList } from './todoList'
 
-export const todoListLocalStorageStartFx = createEffect((state = $todoList) => {
-	if (localStorage.getItem('todoList') === null) {
-		localStorage.setItem('todoList', JSON.stringify(state));
-	} else {
-		setFullTodoList(JSON.parse(localStorage.getItem('todoList') || ''));
-	}
-})
-todoListLocalStorageStartFx()
+const todoList = $todoList.getState()
+if (localStorage.getItem('todoList') === null) {
+	localStorage.setItem('todoList', JSON.stringify(todoList));
+} else {
+	setFullTodoList(JSON.parse(localStorage.getItem('todoList') || ''));
+}
 
 const todoListLocalStorageUpdateFx = createEffect((state: IItemTodo[]) => {
 	localStorage.setItem('todoList', JSON.stringify(state));
