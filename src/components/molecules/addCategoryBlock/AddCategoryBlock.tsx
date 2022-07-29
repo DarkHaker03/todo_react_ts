254a1dd3d5951bc1893efb765d9718a8ddb22e14
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-07-29 14:34:39 +0500

    changed: changed logic of AddCAtegoryBlock

--- src/components/molecules/addCategoryBlock/AddCategoryBlock.tsx
+++ src/components/molecules/addCategoryBlock/AddCategoryBlock.tsx
@@ -13,12 +13,16 @@ import styles from './index.module.css';
 
 export const AddCategoryBlock: FC = () => {
   const [inputValue, setInputValue, inputOnChangeValue] = useInput()
+  const handleClick = () => {
+    categoriesAdd(inputValue);
+    setInputValue('');
+  }
   return (
     <div>
       <h3>Add Item</h3>
       <div className={styles.block}>
         <Input onChange={inputOnChangeValue} value={inputValue} />
-        <Button onClick={() => { categoriesAdd(inputValue); setInputValue('') }}>
+        <Button onClick={handleClick}>
           Add
         </Button>
       </div>
