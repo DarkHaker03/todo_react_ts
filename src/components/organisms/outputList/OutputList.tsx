3045c7688925f1807b79efb518229fc86b981568
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-07-31 11:27:59 +0500

    add: added logic in component OutPutList

--- src/components/organisms/outputList/OutputList.tsx
+++ src/components/organisms/outputList/OutputList.tsx
@@ -1,13 +1,21 @@
+import { useStore } from 'effector-react';
 import { FC } from 'react';
+
+import { $todoList } from '../../../global/store/todoList';
+import { $filteredTodoList } from '../../molecules/searchItemBlock/logic';
 
 import { ItemTodoArray } from '../itemTodoArray/ItemTodoArray';
 
 export const OutputList: FC = () => {
+
+  const todoListLength = useStore($todoList).length;
+  const filteredTodoListLength = useStore($filteredTodoList).length;
+
   console.log("rerender OutputList")
   return (
     <div className="outputList">
-      { true ?
-        true ?
+      { todoListLength !== 0 ?
+        filteredTodoListLength !== 0 ?
           <ItemTodoArray />
           :
           <div>The search didn't yield anything !</div>
